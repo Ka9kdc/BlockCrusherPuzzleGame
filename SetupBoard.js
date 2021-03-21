3b087ebb3c5b5deb23f73583a745c731a44f4c43
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-03-21 18:29:03 -0500

    broke complex functions in to smaller helper functins

--- SetupBoard.js
+++ SetupBoard.js
@@ -1,25 +1,35 @@
 const row = 10;
 
-export default function createGameBoard(rowNum = row, styleNum = 4) {
-	let newBoard = [];
-	const columnNum = rowNum * 5;
-	while (newBoard.length < rowNum) {
-		let rowTiles = new Array(columnNum);
+const checkTiles = (i, rowTiles, newBoard, styleNum, tile) => {
+	while (
+		(rowTiles[i - 1] === tile && rowTiles[i - 2] === tile) ||
+		(newBoard[newBoard.length - 2] &&
+			newBoard[newBoard.length - 1][i] === tile &&
+			newBoard[newBoard.length - 2][i] === tile)
+	) {
+		tile--;
+		if (tile === 0) tile = styleNum;
+	}
+	return tile
+}
+
+const makeRow = (columnNum, newBoard, styleNum) => {
+	let rowTiles = new Array(columnNum);
 		for (let i = 0; i < columnNum; i++) {
 			let tile = Math.ceil(Math.random() * styleNum);
-			while (
-				(i > 1 && rowTiles[i - 1] === tile && rowTiles[i - 2] === tile) ||
-				(newBoard[newBoard.length - 2] &&
-					newBoard[newBoard.length - 1][i] === tile &&
-					newBoard[newBoard.length - 2][i] === tile)
-			) {
-				tile--;
-				if (tile === 0) tile = styleNum;
+			 if ((rowTiles[i - 2]) || newBoard[newBoard.length - 2]){
+				tile = checkTiles(i, rowTiles, newBoard, styleNum, tile)
 			}
 			rowTiles[i] = tile;
 		}
+		return rowTiles
+}
+
+export default function createGameBoard(rowNum = row, styleNum = 4) {
+	let newBoard = [];
+	while (newBoard.length < rowNum) {
+		const rowTiles = makeRow(rowNum * 5, newBoard, styleNum)
 		newBoard.push(rowTiles);
 	}
-
 	return newBoard;
 }
