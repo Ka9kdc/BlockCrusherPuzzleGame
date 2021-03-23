43e42a98b6ec2dc78bbe096f4aac726c2257ae5d
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-03-23 16:25:42 -0500

    ran prettier

--- AboutPage/Team.js
+++ AboutPage/Team.js
@@ -18,37 +18,45 @@ export default function Team() {
 			</div>
 
 			<div>
-                <div id="ListOfMembers">
-                <img src="images/kelsey.png" alt="head shot of Kelsey" id="photoOfMe" />
-				<h2 >Kelsey Schroeder</h2>
+				<div id="ListOfMembers">
+					<img
+						src="images/kelsey.png"
+						alt="head shot of Kelsey"
+						id="photoOfMe"
+					/>
+					<h2>Kelsey Schroeder</h2>
 
-				<div className="contact_links">
-					<a href="https://github.com/Ka9kdc">
+					<div className="contact_links">
+						<a href="https://github.com/Ka9kdc">
+							<img
+								src="images/github-logo.png"
+								alt="Github logo and link to my github"
+							/>
+						</a>
+						<a href="https://www.linkedin.com/in/kelsey-m-schroeder/">
+							<img
+								src="images/linkedin-logo.png"
+								alt="linkedin logo and link to my linked in profile"
+							/>
+						</a>
+						<a href="https://kelsey-schroeder.herokuapp.com">
+							<img
+								src="images/myPorttolio.png"
+								alt="linkedin logo and link to my portfilio website"
+							/>
+						</a>
+					</div>
+				</div>
+				<div>
+					<h3>Technologies Used</h3>
+					<div className="contact_links">
+						<img src="images/react-logo.png" alt="React logo" />
 						<img
-							src="images/github-logo.png"
-							alt="Github logo and link to my github"
+							src="images/html5-logo-31819.png"
+							alt="html5 css3 and javascript logo"
 						/>
-					</a>
-					<a href="https://www.linkedin.com/in/kelsey-m-schroeder/">
-						<img
-							src="images/linkedin-logo.png"
-							alt="linkedin logo and link to my linked in profile"
-						/>
-					</a>
-					<a href="https://kelsey-schroeder.herokuapp.com">
-                    <img
-							src="images/myPorttolio.png"
-							alt="linkedin logo and link to my portfilio website"
-						/>
-					</a>
-				</div></div>
-                <div>
-                    <h3>Technologies Used</h3>
-                    <div className="contact_links">
-                        <img src="images/react-logo.png" alt="React logo" />
-                        <img src="images/html5-logo-31819.png" alt="html5 css3 and javascript logo" />
-                    </div>
-                </div>
+					</div>
+				</div>
 			</div>
 		</div>
 	);
