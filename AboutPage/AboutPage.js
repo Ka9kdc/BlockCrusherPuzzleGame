1487d4fb2e218314914ebf8f62a18aa7d6dac858
Kelsey Schroeder <kelsey.schroeder@att.net>
2021-03-23 15:17:15 -0500

    Ran prettier and eslint

--- AboutPage/AboutPage.js
+++ AboutPage/AboutPage.js
@@ -1,11 +1,24 @@
-import React from 'react'
-import AboveTheFold from './AboveTheFold'
-import Explainer from './Explainer'
-import Team from './Team'
+import React from 'react';
+import AboveTheFold from './AboveTheFold';
+import Explainer from './Explainer';
+import Team from './Team';
 
-
-export default function AboutPage(){
-    return (
-        <div><h1>About Page</h1><div>Above the fold<AboveTheFold /></div><div>Explainer Section<Explainer /></div><div>Team<Team /></div></div>
-    )
+export default function AboutPage() {
+	return (
+		<div>
+			<h1>About Page</h1>
+			<div>
+				Above the fold
+				<AboveTheFold />
+			</div>
+			<div>
+				Explainer Section
+				<Explainer />
+			</div>
+			<div>
+				Team
+				<Team />
+			</div>
+		</div>
+	);
 }
--- AboutPage/Explainer.js
+++ AboutPage/Explainer.js
@@ -1,10 +1,15 @@
-import React from 'react'
+import React from 'react';
 
-export default function Explainer(){
-    return (
-        <div>
-            <p>Explainer section</p><p>
-The next section after the ATF is the Explainer section. This section explains what the application does, how it works, and maybe even some instructions for how to use it. This is important in order to help guide a user through the process of using your application.</p>
-        </div>
-    )
+export default function Explainer() {
+	return (
+		<div>
+			<p>Explainer section</p>
+			<p>
+				The next section after the ATF is the Explainer section. This section
+				explains what the application does, how it works, and maybe even some
+				instructions for how to use it. This is important in order to help guide
+				a user through the process of using your application.
+			</p>
+		</div>
+	);
 }
